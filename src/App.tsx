e3e240d84b6e5cf5ff75a4a3c0620dd05c040708
PROJETOGIROMAX <jefersonconsultor2013@gmail.com>
2026-04-09 19:50:50 -0300

    Update App.tsx 3 TENTATIVA

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import PainPoints from "./components/PainPoints";
-import Benefits from "./components/Benefits";
-import Solution from "./components/Solution";
-import HowItWorks from "./components/HowItWorks";
-import VisualProof from "./components/VisualProof";
-import Offer from "./components/Offer";
-import FinalCTA from "./components/FinalCTA";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfUse from "./pages/TermsOfUse";
 import ScrollToTop from "./components/ScrollToTop";
-import CookieBanner from "./components/CookieBanner";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-dark selection:bg-brand selection:text-white overflow-x-hidden">
+    <Router>
       <ScrollToTop />
-      <Navbar />
-      <Hero />
-      <PainPoints />
-      <Benefits />
-      <Solution />
-      <HowItWorks />
-      <VisualProof />
-      <Offer />
-      <FinalCTA />
-      <Footer />
-      <CookieBanner />
-    </div>
+      <div className="min-h-screen bg-dark selection:bg-brand selection:text-white overflow-x-hidden">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/privacidade" element={<PrivacyPolicy />} />
+          <Route path="/termos" element={<TermsOfUse />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
