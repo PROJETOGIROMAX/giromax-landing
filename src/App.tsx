7fb651e0f0c905b8bd6a0aaa3aa1b59fca7ea03e
PROJETOGIROMAX <jefersonconsultor2013@gmail.com>
2026-04-09 18:50:49 -0300

    adiciona banner de cookies no app

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,29 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import PainPoints from "./components/PainPoints";
-import Benefits from "./components/Benefits";
-import Solution from "./components/Solution";
-import VisualProof from "./components/VisualProof";
-import Offer from "./components/Offer";
-import FinalCTA from "./components/FinalCTA";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfUse from "./pages/TermsOfUse";
+import ScrollToTop from "./components/ScrollToTop";
+import CookieBanner from "./components/CookieBanner";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-dark selection:bg-brand selection:text-white overflow-x-hidden">
-      <Navbar />
-      <main>
-        <Hero />
-        <PainPoints />
-        <Benefits />
-        <Solution />
-        <VisualProof />
-        <Offer />
-        <FinalCTA />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <ScrollToTop />
+      <div className="min-h-screen bg-dark selection:bg-brand selection:text-white overflow-x-hidden">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/privacidade" element={<PrivacyPolicy />} />
+          <Route path="/termos" element={<TermsOfUse />} />
+        </Routes>
+        <Footer />
+        <CookieBanner />
+      </div>
+    </Router>
   );
 }
